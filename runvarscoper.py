49d485126b072303a669c53dfc25784ed9acea9a
matt <mattpbourke@gmail.com>
2014-06-03 18:41:23 +0100

    just a basic working copy, changes to follow then will submit pull request

--- runvarscoper.py
+++ runvarscoper.py
@@ -3,9 +3,9 @@ import webbrowser
 
 class runvarscoperCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		varScoperLink = self.view.settings().get('varscoper_cfm_url', 'http://localhost/tools/varscoper/varScoper.cfm')
+		varScoperLink = self.view.settings().get('varscoper_cfm_url', 'http://127.0.0.1:8888/varscoper/varScoper.cfm?parseCFScript=disabled')
 		if len(self.view.file_name()) > 0:
-			varScoperLink = varScoperLink + '?filePath=' + self.view.file_name()
+			varScoperLink = varScoperLink + '&filePath=' + self.view.file_name()
 			webbrowser.open_new(varScoperLink)
 			msg = "File " + self.view.file_name() + " is running on VarScoper"
 			sublime.status_message(msg)
--- runvarscoperhighlight.py
+++ runvarscoperhighlight.py
@@ -1,51 +1,64 @@
 import sublime, sublime_plugin
 import urllib
+import os
 from xml.dom import minidom
 
-class runvarscoperhighlighCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		varscoper_cfm_url = self.view.settings().get('varscoper_cfm_url', 'http://localhost/tools/varscoper/varScoper.cfm')
+# -- below is originally from sublime linter
+# Select one of the predefined gutter mark themes, the options are:
+# "alpha", "bright", "dark", "hard" and "simple"
+MARK_THEMES = ('alpha', 'bright', 'dark', 'hard', 'simple')
+# The path to the built-in gutter mark themes
+MARK_THEMES_PATH = os.path.join('..', 'sublimeVarScoper', 'gutter_mark_themes')
+# The original theme for anyone interested the previous minimalist approach
+ORIGINAL_MARK_THEME = {
+    'violation': 'dot',
+    'warning': 'dot',
+    'illegal': 'circle'
+}
 
-		varScoperBaseURL = varscoper_cfm_url+'?displayFormat=xml&'
+class eventsstuff(sublime_plugin.EventListener):
+	# these are called on certain events
+	# this is called when someone does the default save of ctrl+s or file menu save etc
+	def on_post_save(self, view='none'):
+		sublime.active_window().run_command("runvarscoperhighlight")
+
+	def on_load(self, view='none'):
+		sublime.active_window().run_command("runvarscoperhighlight")
+
+class runvarscoperhighlightCommand(sublime_plugin.TextCommand):
+
+	def run(self, edit):
+		varscoper_cfm_url = self.view.settings().get('varscoper_cfm_url', 'http://127.0.0.1:8888/varscoper/varScoper.cfm?')
+		varScoperBaseURL  = varscoper_cfm_url+'displayFormat=xml&'
 		if len(self.view.file_name()) > 0:
 			varScoperLink = varScoperBaseURL + 'filePath=' + self.view.file_name()
-			print(varScoperLink)
+			self.view.erase_status('varscoper')
 			try:
-				proxy_support = urllib.request.ProxyHandler({})
-				opener = urllib.request.build_opener(proxy_support)
-				urllib.request.install_opener(opener)
-				http_handler = urllib.request.HTTPHandler()
-				opener = urllib.request.build_opener(http_handler)
-				req = urllib.request.Request(varScoperLink)
-				resp = urllib.request.urlopen(req)
-
-				code = urllib.request.urlopen(varscoper_cfm_url).getcode()
+				resp = urllib.urlopen(varScoperLink)
+				code = urllib.urlopen(varscoper_cfm_url).getcode()
 				if code != 200:
 					sublime.error_message('Server '+varscoper_cfm_url+' non raggiungibile')
 					return
+				data     = resp.read()
 
-				data = resp.read()
-				print(data)
-
-				xmldoc = minidom.parseString(data)
-				itemlist = xmldoc.getElementsByTagName('line_number') 
-				first = False
+				xmldoc   = minidom.parseString(data)
+				itemlist = xmldoc.getElementsByTagName('line_number')
+				first    = False
 				regions = []
 				for s in itemlist :
 					line = self.getText(s.childNodes)
-					print(line)
-					p = self.view.text_point(int(line)-1, 0)
+					p    = self.view.text_point(int(line)-1, 0)
 					regions.append(self.view.full_line(p))
 
-				self.view.add_regions('varscoper', regions, 'string', 'cross')
-				
+				self.view.add_regions('varscoper', regions, 'string', MARK_THEMES_PATH+'/hard-illegal',sublime.HIDDEN)
 				if len(regions) > 0:
 					if(not first):
-						self.view.show(p)
+						# self.view.show(p)
 						first = True
 
 				if len(itemlist) > 0:
-					sublime.error_message("Found " + str(len(itemlist)) + " VarScoper violations")
+					self.view.set_status('varscoper', str(len(itemlist)) + ' Unscoped variables found, please see the gutter icons next to each affected row')
+
 
 			except (urllib.error.HTTPError) as e:
 				sublime.error_message(str(e))
